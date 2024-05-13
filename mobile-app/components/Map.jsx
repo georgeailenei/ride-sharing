f551a8deeb2311951565320e1787502f1e1f8093
George Alexandru Ailenei <george.ailenei@AC-99.local>
2024-05-13 18:55:54 +0300

    get the current position of the user for android

--- mobile-app/components/Map.jsx
+++ mobile-app/components/Map.jsx
@@ -1,17 +1,33 @@
-import React from "react";
-import MapView from "react-native-maps";
+import React, { useState, useEffect } from "react";
+import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View } from "react-native";
-import { clujNapocaRegion } from "./regionsMockupData";
+import { adjudRegion, clujNapocaRegion } from "./regionsMockupData";
+import * as Location from "expo-location";
 
 const TheMap = () => {
+  const [initialRegion, setInitialRegion] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords;
+      setInitialRegion({
+        latitude,
+        longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+
   return (
     <View>
-      <MapView
-        style={styles.map}
-        initialRegion={clujNapocaRegion}
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-      />
+      <MapView style={styles.map} region={clujNapocaRegion} showsTraffic={true}>
+        <Marker
+          coordinate={initialRegion}
+          title="Locatia Cluj but it could be your location and change the icon for android"
+        />
+      </MapView>
     </View>
   );
 };
